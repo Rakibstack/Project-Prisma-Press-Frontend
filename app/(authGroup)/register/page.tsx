4024842e773c53867f0e3ab1a3e4a820a6c05b57
Rakibstack <rh6039511@gmail.com>
2026-08-04 14:20:03 +0600

    chore: Install Shadcn UI Library and Setup project Architecture

--- app/(authGroup)/register/page.tsx
+++ app/(authGroup)/register/page.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const userRegisterPage = () => {
     return (
+        
         <div>
-            
+        
         </div>
     );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "prisma-press frontend",
@@ -14,10 +18,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={` h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       
       <body className="min-h-full flex flex-col">{children}</body>
+      
     </html>
   );
 }
